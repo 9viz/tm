bd570f8b9e11cf19a17cc28b271b448594164792
viz <viz@airmail.cc>
2019-02-07 23:03:03 +0000

    Print usage when an unknown path is used

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,26 @@ use tm;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let usage = || {
+        println!("usage: tm [/path/to/colors/file]");
+    };
+
     if args.len() == 1 {
         println!("error: not enough arguments!");
-        println!("usage: tm [/path/to/colors/file]");
-        process::exit(5)
+        usage();
+        process::exit(5);
     }
 
     // convert the str to a Path
     let colors_path = Path::new(&args[1]);
+
     // get the absolue path
     let colors_path = fs::canonicalize(&colors_path)
-        .unwrap_or_else(|e| { println!("error :{}", e); process::exit(7); });
+        .unwrap_or_else(|e| {
+            println!("error: {}", e);
+            usage();
+            process::exit(7);
+        });
 
     // convert it to string again
     let colors_path = colors_path.to_str().unwrap();
